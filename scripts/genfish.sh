0a1a8551c39368068dd9a7a24492cd9dc20ce066
kar <hi@kar.gs>
2021-08-20 14:11:54 +0200

    update script to follow erc721 base uri

--- scripts/genfish.sh
+++ scripts/genfish.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
 DRAW=../../fishdraw
-OUT=../metadata
+OUTMETA=../token
+OUTIMG=../image
 
 echo "Generating fish"
 
-rm -rf $OUT
-mkdir $OUT
+rm -rf $OUTMETA
+rm -rf $OUTIMG
+mkdir $OUTMETA
+mkdir $OUTIMG
 
 for i in $(seq 1 $1); do
-	mkdir $OUT/$i
+	mkdir $OUTIMG/$i
 	NAME="$(node name.js)"
 	echo "  fish $i ($NAME)"
-	node $DRAW/fishdraw.js --seed "$NAME" --format svg > $OUT/$i/image.svg
-	svgexport $OUT/$i/image.svg $OUT/$i/image-full.png 1920:1180
-	convert $OUT/$i/image-full.png -resize 2048x2048 -background transparent -gravity center -extent 2048x2048 -resize 350x350 $OUT/$i/image-small.png
-	sed "s/FISHID/$i/g" meta-template.json > $OUT/$i/meta.json
-	sed -i "s/FISHNAME/$NAME/g" $OUT/$i/meta.json
+	node $DRAW/fishdraw.js --seed "$NAME" --format svg > $OUTIMG/$i/image.svg
+	svgexport $OUTIMG/$i/image.svg $OUTIMG/$i/image-full.png 1920:1180
+	convert $OUTIMG/$i/image-full.png -resize 2048x2048 -background transparent -gravity center -extent 2048x2048 -resize 350x350 $OUTIMG/$i/image-small.png
+	sed "s/FISHID/$i/g" meta-template.json > $OUTMETA/$i
+	sed -i "s/FISHNAME/$NAME/g" $OUTMETA/$i
 done
